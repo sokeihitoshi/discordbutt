5de0fe2ff07381350dee89224f91fe833ec7c818
Nathan Tsang <nathanhktsang@gmail.com>
2018-01-23 21:29:03 -0800

    added relative path

--- src/discord-bot.py
+++ src/discord-bot.py
@@ -8,8 +8,10 @@ import datetime
 from random import *
 import re
 import yaml
+import os
 
-with open("config.yaml", 'r') as ymlfile:
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, "config.yaml"), 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 username = cfg['mongodb']['user']
 password = cfg['mongodb']['password']
